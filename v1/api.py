578e1c997c748b87d6bc3d57224fd6f16fdab69e
Cole Ogrodnick <patfan8@comcast.net>
2022-10-16 21:52:46 -0500

    clean up

--- v1/api.py
+++ v1/api.py
@@ -3,7 +3,6 @@ from v1.routes import friends, users, posts
 
 router = APIRouter()
 
-# router.include_router(auth.router, tags=['users'], prefix='/users')
 router.include_router(users.router, tags=['Users'], prefix='/users')
 router.include_router(friends.router, tags=[
                       'Friends'], prefix="/friends")
